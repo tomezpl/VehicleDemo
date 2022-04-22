0cc465713031a5f1f1cd56921d812dde0f308910
Tomasz Zając <tomezpl@gmail.com>
2022-04-22 13:30:19 +0100

    start on suspension

--- CarWheel.cs
+++ CarWheel.cs
@@ -16,6 +16,8 @@ public class CarWheel : Spatial
 
     public float WheelRadius = 1f;
 
+    public float MaxSuspensionY = 0f;
+
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -32,6 +34,19 @@ public class CarWheel : Spatial
             AABB wheelAabb = wheelMesh.GetAabb();
             WheelRadius = Mathf.Max(Mathf.Abs(wheelAabb.Size.y), Mathf.Abs(wheelAabb.Size.z));
         }
+
+        // Store the predefined wheel local origin's Y coord as maximum spring length.
+        MaxSuspensionY = Translation.x;
+        Translation *= new Vector3(0f, 1f, 1f);
+    }
+
+    /// <summary>
+    /// Utility method to set the Y coord, useful if the coordinate space is different.
+    /// </summary>
+    /// <param name="distance">Distance along the wheel spring.</param>
+    public void SetSpringDistance(float distance)
+    {
+        Translation = new Vector3(distance, Translation.y, Translation.z);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -46,5 +61,16 @@ public class CarWheel : Spatial
         
         float angularVelocity = (carLinearVelocity / WheelRadius) * ParentCar.Transform.basis.z.Normalized().Dot(-ParentCar.LinearVelocity.Normalized());
         GlobalRotate(GlobalTransform.basis.x.Normalized(), angularVelocity * delta * (FlippedYAxis ? -1f : 1f));
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Transform axle = GetParent<Spatial>().GlobalTransform;
+        Godot.Collections.Dictionary raycast = GetWorld().DirectSpaceState.IntersectRay(axle.origin, axle.origin + axle.basis.z * -MaxSuspensionY);
+        if(raycast != null && raycast.Contains("position"))
+        {
+            Vector3 raycastHit = (Vector3)raycast["position"];
+            SetSpringDistance((raycastHit - axle.origin).Length());
+        }
     }
 }
