bcffb96ec58b6cea1a9d681f17c247ec42b45cf6
Tomasz Zając <tomezpl@gmail.com>
2022-07-10 16:37:06 +0100

    summaries and comments

--- Car.cs
+++ Car.cs
@@ -177,6 +177,10 @@ public partial class Car : RigidBody
         ChaseCam.Translation = new Vector3(Mathf.Sin(yaw) * Mathf.Cos(pitch), -Mathf.Sin(pitch), Mathf.Cos(yaw) * Mathf.Cos(pitch)) * CamDistance;
     }
 
+    /// <summary>
+    /// Gets the peak acceleration achievable by the car. This means it will assume full engine power, no braking and no resistance.
+    /// </summary>
+    /// <returns></returns>
     public Vector3 GetPeakAcceleration()
     {
         return GetLongitudinalForce(1f, 0f, true) / Mass;
@@ -185,7 +189,7 @@ public partial class Car : RigidBody
     /// <summary>
     /// Splits the velocity vector into longitudinal and lateral.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>Z and X speeds in the car's reference frame. Values can be negative to indicate the direction on the axis.</returns>
     public (float lng, float lat) GetVelocitySplit()
     {
         float magnitude = LinearVelocity.Length();
@@ -198,11 +202,22 @@ public partial class Car : RigidBody
         return (Transform.basis.z.Dot(normV) * magnitude, Transform.basis.x.Dot(normV) * magnitude);
     }
 
+    /// <summary>
+    /// Calculates weight exerted on a single tyre.
+    /// </summary>
+    /// <returns></returns>
+    /// <remarks>TODO: This assumes even distribution on all wheels at all times. It should take weight transfer into account.</remarks>
     public Vector3 GetTyreLoad()
     {
-        return Vector3.Down * Weight / 4f;
+        return Vector3.Down * Weight;
     }
 
+    /// <summary>
+    /// Function to calculate lateral force exercised by a tyre based on the slip angle.
+    /// </summary>
+    /// <param name="slipAngleDeg">Slip angle (in degrees) to look up on the curve.</param>
+    /// <returns>Fn,lat (normalized lateral force for <paramref name="slipAngleDeg"/>)</returns>
+    /// <remarks>TODO: This function is derived from a curve specific to a single type of tyre. More would need to be implemented for other tyres. This could allow for things like (but not limited to) implementing tradeoffs between grip, stability and cornering.</remarks>
     public float SlipAngleCurve(float slipAngleDeg)
     {
         float absAngle = Mathf.Abs(slipAngleDeg);
@@ -222,6 +237,12 @@ public partial class Car : RigidBody
         }
     }
 
+    /// <summary>
+    /// Calculates lateral force to apply on the tyre.
+    /// </summary>
+    /// <param name="slipAngle">Slip angle (in radians) of the wheel.</param>
+    /// <param name="tyreLoad">Load (weight) supported by the tyre.</param>
+    /// <returns>Flateral</returns>
     public Vector3 GetLateralForce(float slipAngle, float tyreLoad)
     {
         float slipAngleDeg = Mathf.Rad2Deg(slipAngle);
@@ -246,6 +267,11 @@ public partial class Car : RigidBody
         }
     }
 
+    /// <summary>
+    /// Calculates the target turn radius for a given <paramref name="deltaAngle"/>.
+    /// </summary>
+    /// <param name="deltaAngle">Wheel heading angle (in radians) relative to the car's heading.</param>
+    /// <returns>R</returns>
     public float GetTurnRadius(float deltaAngle)
     {
         return WheelBase / Mathf.Sin(deltaAngle);
@@ -253,7 +279,7 @@ public partial class Car : RigidBody
 
     public float GetTurnRate(float turnRadius)
     {
-        if (turnRadius == 0f)
+        if (turnRadius == 0f || float.IsInfinity(turnRadius))
         {
             return 0f;
         }
@@ -282,6 +308,10 @@ public partial class Car : RigidBody
         return (front, rear);
     }
 
+    /// <summary>
+    /// Gets the delta angle - the wheel's heading relative to the car's heading - in radians.
+    /// </summary>
+    /// <returns>delta</returns>
     public float GetDeltaAngle()
     {
         return WheelTurn * MaxWheelYaw;
@@ -319,32 +349,20 @@ public partial class Car : RigidBody
         Vector3 corneringForce = GetNetCorneringForce(rearLat, frontLat, deltaAngle);
         Vector3 torque = GetCorneringTorque(rearLat, frontLat, deltaAngle);
         (float lng, float lat) velocitySplit = GetVelocitySplit();
-        float direction = Mathf.Sign(velocitySplit.lng);
 
-        //corneringForce *= direction;
-        //torque *= direction;
 
-        //if (Mathf.Abs(GetVelocitySplit().lng) > 0.01f)
+        if (ActiveColliders > 0)
         {
-            /*AddCentralForce(Transform.basis.x * (Mathf.Cos(GetDeltaAngle()) * frontLat.y * 2f * direction));
-
-            AddTorque((rearLat.Normalized() * GetTyreLoad()) * RearAxle.Length() * direction);
-            AddTorque((rearLat.Normalized() * GetTyreLoad()) * RearAxle.Length() * direction);
-            AddTorque(frontLat.Normalized() * GetTyreLoad() * FrontAxle.Length() * Mathf.Cos(GetDeltaAngle()) * direction);
-            AddTorque(frontLat.Normalized() * GetTyreLoad() * FrontAxle.Length() * Mathf.Cos(GetDeltaAngle()) * direction);*/
-
-
-            if (ActiveColliders > 0)
-            {
-                AddCentralForce(RightVector * corneringForce.y);
-                LinearVelocity -= velocitySplit.lat * RightVector * CorneringGrip;
-                AddTorque(-torque);
-            }
-
-            //AddCentralForce(-ForwardVector * direction * corneringForce.y);
+            AddCentralForce(RightVector * corneringForce.y);
+            LinearVelocity -= velocitySplit.lat * RightVector * CorneringGrip;
+            AddTorque(-torque);
         }
     }
 
+    /// <summary>
+    /// Calculates the acceleration in the car's reference frame.
+    /// </summary>
+    /// <returns></returns>
     public Vector3 GetLocalAcceleration()
     {
         float accelMagnitude = Acceleration.Length();
@@ -358,22 +376,49 @@ public partial class Car : RigidBody
         return -1f * new Vector3(RightVector.Dot(accelNorm) * accelMagnitude, UpVector.Dot(accelNorm) * accelMagnitude, ForwardVector.Dot(accelNorm) * accelMagnitude);
     }
 
+    /// <summary>
+    /// Calculates the net cornering force applied to the car's centre of gravity.
+    /// </summary>
+    /// <param name="rearLat">Lateral force from the rear wheels.</param>
+    /// <param name="frontLat">Lateral force from the front wheels.</param>
+    /// <param name="deltaAngle">Delta angle of the front wheels.</param>
+    /// <returns>Fcornering</returns>
     protected Vector3 GetNetCorneringForce(Vector3 rearLat, Vector3 frontLat, float deltaAngle)
     {
-        Vector3 localAcceleration = GetLocalAcceleration();
         return rearLat + (Mathf.Cos(deltaAngle) * frontLat);
     }
 
+    /// <summary>
+    /// Calculates the cornering torque applied to the car's centre of gravity.
+    /// </summary>
+    /// <param name="rearLat">Lateral force from the rear wheels.</param>
+    /// <param name="frontLat">Lateral force from the front wheels.</param>
+    /// <param name="deltaAngle">Delta angle of the front wheels.</param>
+    /// <returns></returns>
     protected Vector3 GetCorneringTorque(Vector3 rearLat, Vector3 frontLat, float deltaAngle)
     {
         var torque = GetCorneringTorqueSplit(rearLat, frontLat, deltaAngle);
         return torque.rear + torque.front;
     }
+
+    /// <summary>
+    /// Calculates the individual contributions to the cornering torque from rear and front axles.
+    /// </summary>
+    /// <param name="rearLat"></param>
+    /// <param name="frontLat"></param>
+    /// <param name="deltaAngle"></param>
+    /// <returns></returns>
     protected (Vector3 rear, Vector3 front) GetCorneringTorqueSplit(Vector3 rearLat, Vector3 frontLat, float deltaAngle)
     {
         return ((-rearLat * RearAxle.Length()), (Mathf.Cos(deltaAngle) * frontLat * FrontAxle.Length()));
     }
 
+    /// <summary>
+    /// Animates weight transfer on the car body.
+    /// </summary>
+    /// <param name="acceleration">Car's acceleration in its reference frame.</param>
+    /// <param name="delta">delta time (seconds since last update frame)</param>
+    /// <remarks>This method applies a purely visual animation, no weight transfer is applied to the wheels or anything that could impact the physics.</remarks>
     protected void AnimateWeightTransfer(Vector3 acceleration, float delta)
     {
         // Weight force acting on each axle
@@ -431,15 +476,23 @@ public partial class Car : RigidBody
         ActiveColliders = Mathf.Max(ActiveColliders - 1, 0);
     }
 
-    // TODO: This only works for a stationary car without weight transfer in play.
+    /// <summary>
+    /// Calculates the max traction force a single tyre can exert.
+    /// </summary>
+    /// <returns>Fmax</returns>
+    /// <remarks>TODO: This only works for a stationary car without weight transfer in play.</remarks>
     protected Vector3 GetMaxTraction()
     {
-        return TyreFriction * Mass * Vector3.Down * GetCarWeight();
+        return TyreFriction * Vector3.Down * GetCarWeight();
     }
 
+    /// <summary>
+    /// Gets the car's total weight.
+    /// </summary>
+    /// <returns>W</returns>
     protected float GetCarWeight()
     {
-        return Mass * GravityScale * 9.81f;
+        return Weight;
     }
 
     protected float GetSideWeight(float acceleration)
@@ -461,11 +514,23 @@ public partial class Car : RigidBody
         return GetAxleWeight(acceleration, RearAxle.Length(), rideHeight);
     }
 
+    /// <summary>
+    /// Calculates the weight on any axle based on <paramref name="axleDistance"/> to the car's centre of gravity.
+    /// </summary>
+    /// <param name="acceleration">The car's local acceleration.</param>
+    /// <param name="axleDistance">Distance from the wheel axle to the car's centre of gravity.</param>
+    /// <param name="rideHeight">Ride height (ie. vertical distance between axle and centre of gravity)</param>
+    /// <returns>Weight on any axle.</returns>
     protected float GetAxleWeight(float acceleration, float axleDistance, float rideHeight)
     {
         return (axleDistance / WheelBase) * GetCarWeight() + (rideHeight / WheelBase) * Mass * acceleration;
     }
 
+    /// <summary>
+    /// Calculates the traction force, which is the car's heading vector scaled by the car's engine input.
+    /// </summary>
+    /// <param name="engineInput">Engine force to apply. Negative values can be used for braking and reversing.</param>
+    /// <returns>Ftraction</returns>
     protected Vector3 GetTractionForce(float engineInput)
     {
         return ForwardVector * engineInput * EngineSpeed;
@@ -489,7 +554,13 @@ public partial class Car : RigidBody
         return -RollingResistance * LinearVelocity;
     }
 
-    protected Vector3 GetBrakeForce(float brakeInput, float engineInput)
+    /// <summary>
+    /// Calculates braking force exerted by the handbrake being applied.
+    /// </summary>
+    /// <param name="brakeInput">Handbrake input value.</param>
+    /// <param name="engineInput">Engine input value.</param>
+    /// <returns></returns>
+    protected Vector3 GetHandbrakeForce(float brakeInput, float engineInput)
     {
         return ForwardVector * Mathf.Abs(LinearVelocity.Normalized().Dot(ForwardVector)) * brakeInput * -BrakeStrength * (GetVelocitySplit().lng > 0f ? -1f : 1f * Mathf.Sign(engineInput));
     }
@@ -500,6 +571,6 @@ public partial class Car : RigidBody
     /// <returns></returns>
     protected Vector3 GetLongitudinalForce(float engineInput, float brakeInput, bool ignoreResistance = false)
     {
-        return ignoreResistance ? (GetTractionForce(engineInput) + GetBrakeForce(brakeInput, engineInput)) : (GetTractionForce(engineInput) + GetBrakeForce(brakeInput, engineInput) + GetDragForce() + GetRollingResistance());
+        return ignoreResistance ? (GetTractionForce(engineInput) + GetHandbrakeForce(brakeInput, engineInput)) : (GetTractionForce(engineInput) + GetHandbrakeForce(brakeInput, engineInput) + GetDragForce() + GetRollingResistance());
     }
 }
